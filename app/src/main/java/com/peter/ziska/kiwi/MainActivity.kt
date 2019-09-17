bd2758591b2f262d0d70ba12355f33b2777b4388
Peter Žiška <peter.ziska@ahead-itec.com>
2019-09-17 18:57:46 +0200

    Initial commit

---
+++ app/src/main/java/com/peter/ziska/kiwi/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.peter.ziska.kiwi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.peter.ziska.kiwi.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
